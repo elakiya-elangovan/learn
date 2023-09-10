92c7296b82f150fa68a4f17486c1c2c4030476ad
Elakiya Elangovan <elakiyae21ec@psnacet.edu.in>
2023-09-10 23:35:16 +0530

    puriyura maari code eluthanu

--- sample.c
+++ sample.c
@@ -1,20 +1,10 @@
 #include<stdio.h>
-
-void fun()
-{
-    printf("GUD MRNG \n");
-    void call();
-    call();
-}
-
-void call()
-{
-    printf("HI HOW ARE \n ");
-}
-
-
 void main()
 {
-    fun();
+    int a,b;
+    printf("%d%d",a);      //a=0 b=garbage value
+    printf("%d%d",12);     //a=12 b=garbage value
+    scanf("%d%d",&a,&b);   //getting 2 input from user
+    printf("%d%d",a);      //a=user given value b=0(value of b is gvn but not printed) 
 
 }
